d0ba7fc9f48e4a5cc31bc1f189fc313ff0abfb05
mendesflavia <mendesflavia@hotmail.com>
2021-03-02 13:36:08 -0300

    arrunado ex

--- 10 - Copia.js
+++
@@ -1,9 +0,0 @@
-//Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR. */
-
-// DESAFIO 10:
-alert=("Desafio 10")
-//VARIAVEIS:
-let numero = Number(prompt('Digite um número '));
-let resultado = numero % 2 ===0? 'par':'impar'; 
-//RESULTADO
-alert(`número ${numero} é ${resultado}`); 
